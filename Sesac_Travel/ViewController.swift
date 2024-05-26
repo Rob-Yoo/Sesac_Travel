e3c2d9e20e43cc7fac279bfc82ce041a885c61a4
Rob-Yoo <jin12243@naver.com>
2024-05-26 14:19:23 +0900

    프로젝트 세팅 완료

---
+++ Sesac_Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sesac_Travel
+//
+//  Created by Jinyoung Yoo on 5/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
