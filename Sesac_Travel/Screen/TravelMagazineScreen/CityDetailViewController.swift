a21455134f52429b72bac1965afdf27e76204579
Rob-Yoo <jin12243@naver.com>
2024-05-28 21:45:55 +0900

    RowHeight 설정 코드 UITableViewDelegate 메서드 활용으로 변경

--- Sesac_Travel/Screen/TravelMagazineScreen/CityDetailViewController.swift
+++ Sesac_Travel/Screen/TravelMagazineScreen/CityDetailViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CityDetailViewController: UIViewController, UITableViewDataSource {
+class CityDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var cityDetailTableView: UITableView!
     
@@ -19,6 +19,7 @@ class CityDetailViewController: UIViewController, UITableViewDataSource {
 
         super.viewDidLoad()
         self.cityDetailTableView.backgroundColor = .white
+        self.cityDetailTableView.delegate = self
         self.cityDetailTableView.dataSource = self
         self.cityDetailTableView.register(UINib(nibName: cityDetailTableViewCell, bundle: nil), forCellReuseIdentifier: cityDetailTableViewCell)
         self.cityDetailTableView.register(.init(nibName: adTableViewCell, bundle: nil), forCellReuseIdentifier: adTableViewCell)
@@ -28,18 +29,24 @@ class CityDetailViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.travelList.count
     }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let travel = self.travelList[indexPath.row]
+        
+        return (travel.ad) ? 75 : 120
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(#function)
         let travel = self.travelList[indexPath.row]
         let id = (travel.ad) ? ADTableViewCell.reusableIdentifer : CityDetailTableViewCell.reusableIdentifer
         let cell = self.cityDetailTableView.dequeueReusableCell(withIdentifier: id, for: indexPath)
         
-        guard let travelDetailCell = cell as? CityDetailTableViewCell else {
+        guard let travelDetailCell = cell as? TravelDetailCellProtocol else {
             return UITableViewCell()
         }
         
         travelDetailCell.configureCellData(data: travel)
-        self.cityDetailTableView.rowHeight = (travel.ad) ? 75 : 120
         return cell
     }
 }
